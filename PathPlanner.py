7a0e4a6b945c2b12c3b4656a75054627ea399c9a
CatastrophicBing <CatastrophicBing@gmail.com>
2022-01-29 14:25:30 -0500

    set heuristic = 0 I believe

--- PathPlanner.py
+++ PathPlanner.py
@@ -202,96 +202,16 @@ class PathPlanner:
         addx = [1, -1, 0, 0, 1, 1, -1, -1]
         addy = [0, 0, 1, -1, 1, -1, -1, 1]
         for i in range(len(addx)):
-            if PathPlanner.is_cell_walkable(mapdata, x + addx[i], y + addy[i]):
-                neighbors.append((x + addx[i], y + addy[i]))
+            try:
+               neighbors.append((x + addx[i], y + addy[i]))
+               break
+            except Exception as err:
+                print("Not a valid index!")
         return neighbors
 
 
-
-    @staticmethod
-    def request_map():
-        """
-        Requests the map from the map server.
-        :return [OccupancyGrid] The grid if the service call was successful,
-                                None in case of error.
-        """
-        ### REQUIRED CREDIT
-        rospy.loginfo("Requesting the map")
-        rospy.wait_for_service('/dynamic_map')
-        try:
-            GetMapData = rospy.ServiceProxy('/dynamic_map', GetMap)
-            rospy.loginfo("Map obtained")
-            return GetMapData().map
-        except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
-
-    def calc_cspace(self, mapdata, padding):
-        """
-        Calculates the C-Space, i.e., makes the obstacles in the map thicker.
-        Publishes the list of cells that were added to the original map.
-        :param mapdata [OccupancyGrid] The map data.
-        :param padding [int]           The number of cells around the obstacles.
-        :return        [OccupancyGrid] The C-Space.
-        """
-        ### REQUIRED CREDIT
-        rospy.loginfo("Calculating C-Space")
-        cSpaceMsg = GridCells()
-        ## Go through each cell in the occupancy grid
-        ## Inflate the obstacles where necessary
-        tolerance = 50 # Defines what is a open and what is obstacle
-        cspacemap = list(mapdata.data)
-        # print(cspacemap)
-        listOfObstacles = []
-        for i in range(len(mapdata.data)):
-            if(mapdata.data[i] > tolerance):
-                listOfObstacles.append(i)
-        for i in listOfObstacles:
-            grid = PathPlanner.index_to_grid(mapdata, i)
-            for j in range(padding+1):
-                for k in range(padding+1):
-                    if grid[0] + j < mapdata.info.width and grid[0] - j >= 0 and grid[1] + k < mapdata.info.height and grid[1] - k >= 0:
-                        # Takes care of both sides of the grid decrasing to the center and fills each space with 100 to mark as Cspace
-                        cspacemap[PathPlanner.grid_to_index(mapdata, grid[0] + j, grid[1] + k)] = 100
-                        # print(grid[0], grid[1], PathPlanner.grid_to_index(mapdata, grid[0] + j, grid[1] + k))
-                        cspacemap[PathPlanner.grid_to_index(mapdata, grid[0] + j, grid[1] - k)] = 100
-                        cspacemap[PathPlanner.grid_to_index(mapdata, grid[0] - j, grid[1] + k)] = 100
-                        cspacemap[PathPlanner.grid_to_index(mapdata, grid[0] - j, grid[1] - k)] = 100
-                # This is a separate way to do Cspace which might take more time than above
-                # for j in range((padding*2+1)**2):
-                #     ychange = math.floor(j/(padding*2+1))*mapdata.info.width
-                #     xchange = j%(padding*2+1)
-                #     try:
-                #         cspacemap[i+ychange+xchange-padding*mapdata.info.width-padding] = 100
-                #     except:
-                #         print("Index out of bounds")
-        ## Create a GridCells message and publish it
-        # print(cspacemap)
-        Cells = []
-        # Creates our GridCells message using a cells message and just copies ove the data from the cspace we gathered into the correct datatype
-        for k in range(len(cspacemap)):
-            if cspacemap[k] > tolerance:
-                gridCoord = PathPlanner.index_to_grid(mapdata, k)
-                Cells.append(PathPlanner.grid_to_world(mapdata, gridCoord[0], gridCoord[1]))
-        # Makes header and cells message to publish as a GridCell
-        cSpaceMsg.header.frame_id = "map"
-        cSpaceMsg.header.stamp = rospy.Time.now()
-        cSpaceMsg.cell_width = mapdata.info.resolution
-        cSpaceMsg.cell_height = mapdata.info.resolution
-        cSpaceMsg.cells = Cells
-        self.pubC.publish(cSpaceMsg)
-        ## Will return 3 numbers,
-        ## Return the C-space
-        rospy.loginfo("C-Space calculated")
-        #print(cspacemap)
-
-        finalCMap = mapdata
-        finalCMap.data = tuple(cspacemap)
-        return finalCMap
-
-
-
     def a_star(self, mapdata, start, goal):
-        """The start and goal are a tuple in grid format, mappdata = Cspacedata"""
+        """The start and goal are a tuple in grid format, mapdata is a matrix of size x,y"""
         ### REQUIRED CREDIT
         print("Executing A* from (%d,%d) to (%d,%d)" % (start[0], start[1], goal[0], goal[1]))
         frontier = PriorityQueue()
@@ -309,7 +229,7 @@ class PathPlanner:
         for priorityTuple in frontierStuff:
             gridTuple = priorityTuple[1]
             # print(gridTuple)
-            frontierCells.append(PathPlanner.grid_to_world(mapdata, gridTuple[0], gridTuple[1]))
+            frontierCells.append(gridTuple[0], gridTuple[1])
 
 
         #expanded cells to show pathed frontiers
@@ -321,7 +241,7 @@ class PathPlanner:
             current = frontier.get()
 
             # update the frontiers visited just to let us see visually if we want
-            expandedCells.append(PathPlanner.grid_to_world(mapdata, current[0], current[1]))
+            expandedCells.append(current[0], current[1])
 
             # If we have reached the goal, leave
             if current == goal:
@@ -329,7 +249,7 @@ class PathPlanner:
 
             #Add viable children to frontier
             for next in PathPlanner.neighbors_of_8(mapdata, current[0], current[1]):
-                new_cost = cost_so_far[current] + PathPlanner.euclidean_distance(current[0], current[1], next[0], next[1])
+                new_cost = 0 # cost_so_far[current] + PathPlanner.euclidean_distance(current[0], current[1], next[0], next[1])
                 if next not in cost_so_far or new_cost < cost_so_far[next]:
                     cost_so_far[next] = new_cost
                     priority = new_cost + PathPlanner.euclidean_distance(current[0], current[1], goal[0], goal[1])
@@ -340,10 +260,7 @@ class PathPlanner:
                     frontierCells = []
                     for priorityTuple in frontierStuff:
                         gridTuple = priorityTuple[1]
-                        frontierCells.append(PathPlanner.grid_to_world(mapdata, gridTuple[0], gridTuple[1]))
-                    frontierMsg.cells = frontierCells
-                    self.pubF.publish(frontierMsg)
-                    rospy.sleep(0.1)
+                        frontierCells.append(gridTuple[0], gridTuple[1])
 
                     #add parent to came_from table
                     came_from[next] = current
@@ -361,73 +278,9 @@ class PathPlanner:
 
         #reverse path to make it so the first element is the start
         path = path[::-1]
-        rospy.loginfo("A* completed")
+        print("A* completed")
         #print(path)
         return path
-
-
-
-    @staticmethod
-    def optimize_path(path):
-        """
-        Optimizes the path, removing unnecessary intermediate nodes.
-        :param path [[(x,y)]] The path as a list of tuples (grid coordinates)
-        :return     [[(x,y)]] The optimized path as a list of tuples (grid coordinates)
-        """
-        ### EXTRA CREDIT
-        rospy.loginfo("Optimizing path")
-
-        #Done by doing y = mx+b and checking if on line
-        m = 1 #slope
-        b = 0 #constant
-        l = 1 #y slope
-        t = 0 #y constant
-        newPath = path[:]
-        counter = 0
-        for cell in path:
-            if cell != path[-1]:
-                nextCell = path[counter + 1]
-                if nextCell[0] - cell[0] != 0:
-                    if m * nextCell[0] + b != nextCell[1]:#if the slope has changed, make a new one!
-                        m = (nextCell[1] - cell[1]) / (nextCell[0] - cell[0])
-                        b = nextCell[1] - m * nextCell[0]
-                    else:
-                        if counter != 0: # since the optimizer starts at 0 and we want to make it as helpful as possible, this is necessary
-                            newPath.remove(cell)# to keep the first position(start) as a node that has to be visited
-                else:
-                    if l * nextCell[1] + t != nextCell[0]:
-                        l = (nextCell[0] - cell[0]) / (nextCell[1] - cell[1])
-                        t = nextCell[0] - l * nextCell[1]
-                    else:
-                        if counter != 0:
-                            newPath.remove(cell)
-            counter += 1
-            print(newPath)
-        rospy.loginfo("Path Optimized")
-        print(path)
-        print("New Path is: ")
-        print(newPath)
-        return newPath
-
-
-
-
-    def path_to_message(self, mapdata, path):
-        """
-        Takes a path on the grid and returns a Path message.
-        :param path [[(int,int)]] The path on the grid (a list of tuples)
-        :return     [Path]        A Path message (the coordinates are expressed in the world)
-        """
-        ### REQUIRED CREDIT
-        rospy.loginfo("Returning a Path message")
-        thePath = Path()
-        thePath.header.frame_id = "map"
-        thePath.header.stamp = rospy.Time.now()
-        thePath.poses = PathPlanner.path_to_poses(mapdata, path) #list of stamped poses
-
-        return thePath
-
-
 
 
     def plan_path(self, msg):
