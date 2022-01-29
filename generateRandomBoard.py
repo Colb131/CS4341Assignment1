a91e385aeb759b611ec2ee1787658038e196dd91
Colb131 <cfrechette@wpi.edu>
2022-01-29 14:47:01 -0500

    Made getBoard a function

--- generateRandomBoard.py
+++ generateRandomBoard.py
@@ -1,29 +1,26 @@
 import numpy as np
 from numpy import *
 
-a = array([[4, "G", 4, 6],
-           [2, 9, 9, 6],
-           [1, 4, "S", 3]])
-
-arrayCols = 4
-arrayRows = 3
-
-array = np.random.randint(10, size=(arrayRows, arrayCols))
-
-goalCol = np.random.randint(0, arrayCols - 1)
-goalRow = np.random.randint(0, arrayRows - 1)
-
-array[goalCol][goalRow] = -1
-
-
-def placeS():
+def placeS(arrayCols, arrayRows, board):
     startCol = np.random.randint(0, arrayCols - 1)
     startRow = np.random.randint(0, arrayRows - 1)
-    if array[startCol][startRow] == -1:
-        placeS()
+    if board[startCol][startRow] == -1:
+        placeS(arrayCols, arrayRows, board)
     else:
-        array[startCol][startRow] = -2
+        board[startCol][startRow] = -2
 
-placeS()
 
-print(array)
+def getBoard():
+    arrayCols = 4
+    arrayRows = 3
+
+    board = np.random.randint(10, size=(arrayRows, arrayCols))
+
+    goalCol = np.random.randint(0, arrayCols - 1)
+    goalRow = np.random.randint(0, arrayRows - 1)
+
+    board[goalCol][goalRow] = -1
+
+    placeS(arrayCols, arrayRows, board)
+
+    print(board)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import PathPlanner as Path_Planner
+import generateRandomBoard
 import generateRandomBoard as Board_Gen
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    boardGen = Board_Gen()
+    boardGen = generateRandomBoard.getBoard()
     pPlaner = Path_Planner()
     board = boardGen.gen_board()
     path = pPlaner.plan_path(board)
