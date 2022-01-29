bc3f5ec624fac1c675d1f92a1bbe16ecf3b2fae2
CatastrophicBing <CatastrophicBing@gmail.com>
2022-01-29 14:36:44 -0500

    setting up skeleton

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import PathPlanner as path_planner
+import PathPlanner as Path_Planner
+import generateRandomBoard as Board_Gen
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    boardGen = Board_Gen()
+    pPlaner = Path_Planner()
+    board = boardGen.gen_board()
+    path = pPlaner.plan_path(board)
     print('Hi')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
