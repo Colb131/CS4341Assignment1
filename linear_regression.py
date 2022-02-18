e1c95a6888d0495d177b5bae82f0052e18ae6f52
BlueOcean090 <kashara>
2022-02-18 17:52:43 -0500

    added to the skeleton for analysis

---
+++ linear_regression.py
@@ -0,0 +1,14 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+"""
+code taken from https://realpython.com/linear-regression-in-python/
+"""
+
+# TODO read in file from csv to arrays
+
+# TODO reformat the arrays so the scores are in 1 array and the x_dist and y_dist are in pairs in another array
+
+# TODO find out how to get the multiple factors of linear regression to work
+
+# TODO export values to the learned_values file
